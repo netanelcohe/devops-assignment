258e555e678a8bd18a4d7ad66c4e2a82bd314bbb
netanelcohe <87936443+netanelcohe@users.noreply.github.com>
2024-12-10 09:39:56 +0200

    Update dock-jenk-install.sh
    add comments

--- dock-jenk-install.sh
+++ dock-jenk-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# Set up Docker's apt repository
+
+###################################
+######  Docker installation #######
+###################################
+
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -10,8 +14,13 @@ echo \
 $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-# install the latest version
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+####################################
+######  Jenkins installation #######
+####################################
+
 sudo apt update && sudo apt upgrade -y
 
 # Add Jenkins repository key 
